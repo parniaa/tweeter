038fdc207e2b2e57bbc1714384e2be08dd6f8c93
paarmin <parniaa@gmail.com>
2021-02-04 14:45:31 -0500

    worlds counter fixed with reverse

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   
   $(".i2").keyup(function() {
     let inputValue = $(this).val().length;
-    $(".i3i2").val(inputValue + 1);
+    $(".i3i2").val( 140 - (inputValue ));
     if (inputValue > 139) {
       $(".i3i2").css("color", "red");
     } else {
